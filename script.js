44f1c12c1f845bf741913bf60da20739168c9b15
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-23 15:02:16 -0500

    Add basic drawing and styling

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const container = document.querySelector('#container');
 
 let gridDensity = 16;
+let color = 'black';
+const size = document.querySelector('#container').offsetWidth;
+let cellSize = size/gridDensity;
 
-for (let i = 0; i < gridDensity; i++) {
-  const column = document.createElement('div');
-  column.setAttribute('class', 'col')
-  container.appendChild(column);
-}
-const columns = document.querySelectorAll('.col');
-columns.forEach(column => {
+function makeGrid (gridDensity) {
   for (let i = 0; i < gridDensity; i++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    column.appendChild(row);
+    const column = document.createElement('div');
+    column.style.height = `${cellSize}px`;
+    column.style.width = `${cellSize}px`;
+    column.setAttribute('class', 'col')
+    container.appendChild(column);
   }
-})
+  const columns = document.querySelectorAll('.col');
+  columns.forEach(column => {
+    for (let i = 0; i < gridDensity; i++) {
+      const cell = document.createElement('div');
+      cell.setAttribute('class', 'cell');
+      cell.style.height = `${cellSize}px`;
+      cell.style.width = `${cellSize}px`;
+      column.appendChild(cell);
+      cell.addEventListener('mouseover', draw);
+    }
+  });
+
+}
+makeGrid(gridDensity);
+
+function draw() {
+  this.style.backgroundColor = `${color}`;
+}
