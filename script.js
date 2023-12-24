f5ee5adcf49ba9c5d1df7c6c1aa319614afcd581
ruddyduckling <1039781+ruddyduckling@users.noreply.github.com>
2023-12-24 11:10:19 -0500

    Complete version 1

--- script.js
+++ script.js
@@ -1,33 +1,96 @@
+let testColor = 'magenta';
+
+
+
+
+
 const container = document.querySelector('#container');
+const size = document.querySelector('#container').offsetWidth;
 
 let gridDensity = 16;
-let color = 'black';
-const size = document.querySelector('#container').offsetWidth;
-let cellSize = size/gridDensity;
+let startColor = '#C5CDBD';
+let drawColor = 'black';
 
-function makeGrid (gridDensity) {
-  for (let i = 0; i < gridDensity; i++) {
+const sizeButton = document.querySelector('.size-btn');
+sizeButton.addEventListener('click', () => {
+  removeCells();
+  makeGrid(getGridDimensions(), startColor);
+});
+
+function getGridDimensions() {
+  gridDensity = prompt(`how many squares per side? (1-100)`);
+
+  while (Math.sign(gridDensity) !== 1  || gridDensity > 100) {
+    gridDensity = prompt('Please choose a positive number from 1-100.')
+  } 
+
+  console.log(`New Grid Density = ${gridDensity}x${gridDensity}`);
+  return gridDensity;
+}
+
+function removeCells() {
+  let cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.remove();
+  });
+}
+
+makeGrid(gridDensity, startColor);
+
+function makeGrid (gridDensity, color) {
+
+
+ /*  for (let i = 0; i < gridDensity; i++) {
     const column = document.createElement('div');
-    column.style.height = `${cellSize}px`;
-    column.style.width = `${cellSize}px`;
-    column.setAttribute('class', 'col')
-    container.appendChild(column);
-  }
-  const columns = document.querySelectorAll('.col');
-  columns.forEach(column => {
-    for (let i = 0; i < gridDensity; i++) {
+    container.appendChild(column)
+    for (let j = 0; j < gridDensity; j++) {
       const cell = document.createElement('div');
       cell.setAttribute('class', 'cell');
+      let cellSize = size/gridDensity;
       cell.style.height = `${cellSize}px`;
       cell.style.width = `${cellSize}px`;
-      column.appendChild(cell);
+      cell.style.backgroundColor = `${color}`
       cell.addEventListener('mouseover', draw);
+      column.appendChild(cell);
     }
-  });
+  } */
 
+
+  for (let i = 0; i < gridDensity**2; i++) {
+    const cell = document.createElement('div');
+    cell.setAttribute('class', 'cell');
+    let cellSize = size/gridDensity;
+    cell.style.height = `${cellSize}px`;
+    cell.style.width = `${cellSize}px`;
+    cell.style.backgroundColor = `${color}`
+    container.appendChild(cell);
+    cell.addEventListener('mouseover', draw);
+  }
 }
-makeGrid(gridDensity);
+
 
 function draw() {
-  this.style.backgroundColor = `${color}`;
+  this.style.backgroundColor = `${drawColor}`;
+}
+
+const eraseButton = document.querySelector('.erase');
+eraseButton.addEventListener('click', () => {
+  erase();
+});
+
+
+function erase() {
+  let eraseCells = document.querySelectorAll('.cell');
+  eraseCells.forEach((cell) => {
+    cell.style.backgroundColor = `${startColor}`;
+  });
+}
+
+const colorButton = document.querySelector('.color');
+colorButton.addEventListener('click', () =>{
+  drawColor = prompt('New color hexcode: ');
+});
+
+function showRuddy () {
+  console.log('ruddyduck');
 }
